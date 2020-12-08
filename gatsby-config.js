1628e75b07fc202246fcfe9f8975ef6593720eb9
Andrea Romagnoli <andrea.romagnoli@gpi.it>
2020-12-08 23:02:32 +0100

    aggiunto i posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,14 @@ module.exports = {
     title: "Aroma Tea"
   },
   plugins: [
-    'gatsby-plugin-netlify-cms'
+      'gatsby-plugin-netlify-cms',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: 'blog',
+          path: 'src/blog'
+        }
+      },
+      'gatsby-transformer-remark'
     ]
 };
